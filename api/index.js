e22518d38c31fd3ae344dcc38ec342e8df6dfe66
Nelson Chinedu <egwuchinedu69@gmail.com>
2019-03-06 14:25:15 -0800

    update index.js file

--- api/index.js
+++ api/index.js
@@ -1,8 +1,9 @@
 import express from 'express';
 import db from './config/database';
 import mealsRoute from './routes/meal.route';
-// import menuRoute from './routes/menu.route';
-// import orderRoute from './routes/order.route';
+import menuRoute from './routes/menu.route';
+import orderRoute from './routes/order.route';
+import user from './routes/user.route';
 
 
 const PORT = process.env.PORT || 3000;
@@ -14,8 +15,9 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.json({ extended: false }));
 
 app.use('/api/v1/meals', mealsRoute);
-// app.use('/api/v1/menu', menuRoute);
-// app.use('/api/v1/orders', orderRoute);
+app.use('/api/v1/menu', menuRoute);
+app.use('/api/v1/orders', orderRoute);
+app.use('/auth', user);
 
 app.get('/', (req, res) => {
   res.send('Welcome to Mealitious foodie');
