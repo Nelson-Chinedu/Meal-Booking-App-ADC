a098c9bc81d735baad8dca3ece5d3a5bd65b3975
Nelson Chinedu <egwuchinedu69@gmail.com>
2019-03-05 20:37:23 -0800

    init meal.route

--- api/routes/meal.route.js
+++ api/routes/meal.route.js
@@ -5,7 +5,7 @@ const router = Router();
 router.get('/', mealController.fetchAllMeal);
 router.get('/:id', mealController.getMeal);
 router.post('/', mealController.addMeal);
-router.put('/:id', mealController.updateMeal);
-router.delete('/:id', mealController.deleteMeal);
+// router.put('/:id', mealController.updateMeal);
+// router.delete('/:id', mealController.deleteMeal);
 
 export default router;
